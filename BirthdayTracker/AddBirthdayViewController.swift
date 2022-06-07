119e43f1d0fb9fd877321700eee410f4e42b4463
Sokol8807 <sokol8807@gmail.com>
2022-06-07 21:08:26 +0300

    выполнил условия главы 10

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -18,9 +18,35 @@ class AddBirthdayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        birthdatePicker.maximumDate = Date()
+    
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        
+        let birthdate = birthdatePicker.date
+        
+        let newBirthday = Birhday(firstName: firstName, lastName: lastName, birthdate: birthdate)
+        
+        print("Cоздана новая запись о ДР")
+        print("Имя: \(newBirthday.firstName)")
+        print("Фамилия: \(newBirthday.lastName)")
+        print("День рождения: \(birthdate)")
+    }
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true)
+        
+        
+        print("Нажата кнопка выхода")
+    }
+    
+    
 }
 
