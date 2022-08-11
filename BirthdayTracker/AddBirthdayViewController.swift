b7abe4f428af207cd3ad25179d9fa19029e7e005
Sokol8807 <sokol8807@gmail.com>
2022-08-11 12:58:16 +0300

    Доделал 13 главу

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -6,21 +6,15 @@
 //
 
 import UIKit
+import CoreData
+import  UserNotifications
 
-protocol AddBirthdayViewControllerDelegate {
-    
-    func addBirthdayViewController(_ AddBirthdayViewController: AddBirthdayViewController, didAddBirthdaybirthday: Birthday)
-    
-}
 
 class AddBirthdayViewController: UIViewController {
 
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var birthdatePicker: UIDatePicker!
-    
-    
-    var delegate: AddBirthdayViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,25 +28,51 @@ class AddBirthdayViewController: UIViewController {
     @IBAction func saveTapped(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
-        
         let birthdate = birthdatePicker.date
         
-        let newBirthday = Birthday(firstName: firstName, lastName: lastName, birthdate: birthdate)
-  
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
-        delegate?.addBirthdayViewController(self, didAddBirthdaybirthday: newBirthday)
+        let newBirthday = Birthday(context: context)
+        newBirthday.firstName = firstName
+        newBirthday.lastName = lastName
+        newBirthday.birthdate = birthdate //as NSDate? as Date?
+        newBirthday.birthdayId = UUID().uuidString
+        
+        if let uniqueId = newBirthday.birthdayId {
+            print("birthdayId: \(uniqueId) ")
+        }
+        do {
+            try context.save()
+            let message = "Сегодня \(firstName)  \(lastName) празднует ДР"
+            let content = UNMutableNotificationContent()
+            content.body = message
+            content.sound = UNNotificationSound.default
+            var dateComponents = Calendar.current.dateComponents([.month, .day],
+                                                                from: birthdate)
+            dateComponents.hour = 8
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents,
+                                                        repeats: true)
+            if let identifier = newBirthday.birthdayId {
+                let request = UNNotificationRequest(identifier: identifier,
+                                                    content: content,
+                                                    trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                center.add(request, withCompletionHandler: nil)
+            }
+        } catch let error {
+            
+            print("Не удалось сохранить из-за ошбики \(error)")
+            
+        }
+        
         
         dismiss(animated: true, completion: nil)
-        
-        
-
     }
     
     @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
         
         dismiss(animated: true)
-        
-        
         print("Нажата кнопка выхода")
     }
     
--- BirthdayTracker/AppDelegate.swift
+++ BirthdayTracker/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound],
+                                    completionHandler: {(granted, error) in
+            
+            if granted {
+                print("Разрешение на отправку уведомления получено!")
+            }   else {
+                print("В разршение на отправку уведовмления отканно")
+            }
+        })
+        
         return true
     }
 
@@ -27,20 +38,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        
     }
 
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
+     
         let container = NSPersistentContainer(name: "BirthdayTracker")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
